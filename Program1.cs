cb0500a0f7f542161aa35b2203426ca9c24e3abf
Alperen Çıtak <citak_alperen@outlook.com>
2023-10-12 10:52:37 +0300

    PrimeNumberLesson

---
+++ Program1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsalDeneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 9;
+
+
+            if (sayi == 2)
+            {
+                Console.WriteLine("This is a Prime Number");
+            }
+            if (sayi < 2)
+            {
+                Console.WriteLine("This is not a Prime Number");
+            }
+
+            for (int i = 2; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    Console.WriteLine("This is not a Prime Number");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("This is a Prime Number");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
